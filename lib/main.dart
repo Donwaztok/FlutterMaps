c53bc7517103fe38e47eee2f68fe20e2b3aa5e4f
Donwaztok <igor_irm_13@hotmail.com>
2020-01-21 16:18:32 -0300

    Adicionado caminho percorrido

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:location_permissions/location_permissions.dart';
+import 'package:location/location.dart';
 
 const double CAMERA_ZOOM = 13;
 const double CAMERA_TILT = 0;
@@ -13,21 +14,11 @@ const LatLng DEST_LOCATION = LatLng(-23.5747, -46.6369);
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,15 +29,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,24 +37,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Completer<GoogleMapController> _controller = Completer();
-  // this set will hold my markers
   Set<Marker> _markers = {};
-  // this will hold the generated polylines
   Set<Polyline> _polylines = {};
-  // this will hold each polyline coordinate as Lat and Lng pairs
   List<LatLng> polylineCoordinates = [];
-  // this is the key object - the PolylinePoints
-  // which generates every polyline between start and finish
+  List<LatLng> path = [];
   PolylinePoints polylinePoints = PolylinePoints();
+  static LatLng latLng;
+  LocationData currentLocation;
 
   static const String googleAPIKey = 'AIzaSyASspOu0rSMaUOR28MNDZlkEhspkWnKkJo';
-
 
   @override
   void initState() {
     super.initState();
   }
-
 
   void onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
@@ -83,47 +61,53 @@ class _MyHomePageState extends State<MyHomePage> {
   void setMapPins() {
     setState(() {
       // source pin
-      _markers.add(Marker(
-          markerId: MarkerId('sourcePin'),
-          position: SOURCE_LOCATION));
+      _markers.add(
+          Marker(markerId: MarkerId('sourcePin'), position: SOURCE_LOCATION));
       // destination pin
-      _markers.add(Marker(
-          markerId: MarkerId('destPin'),
-          position: DEST_LOCATION));
+      _markers
+          .add(Marker(markerId: MarkerId('destPin'), position: DEST_LOCATION));
     });
   }
 
-  void requestPermission() async{
-    PermissionStatus permission = await LocationPermissions().requestPermissions();
+  void requestPermission() async {
+    PermissionStatus permission =
+        await LocationPermissions().requestPermissions();
+    var location = new Location();
+    location.onLocationChanged().listen((currentLocation) {
+      setState(() {
+        latLng = LatLng(currentLocation.latitude, currentLocation.longitude);
+        path.add(latLng);
+      });
+    });
   }
 
   setPolylines() async {
     List<PointLatLng> result = await polylinePoints?.getRouteBetweenCoordinates(
         googleAPIKey,
+        DEST_LOCATION.latitude,
         SOURCE_LOCATION.latitude,
         SOURCE_LOCATION.longitude,
-        DEST_LOCATION.latitude,
         DEST_LOCATION.longitude);
     if (result.isNotEmpty) {
-      // loop through all PointLatLng points and convert them
-      // to a list of LatLng, required by the Polyline
       result.forEach((PointLatLng point) {
         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
       });
     }
 
     setState(() {
-      // create a Polyline instance
-      // with an id, an RGB color and the list of LatLng pairs
+      Polyline polylinePath = Polyline(
+          polylineId: PolylineId("polyPath"),
+          color: Color.fromARGB(180, 90, 90, 90),
+          points: path);
+
       Polyline polyline = Polyline(
+
           polylineId: PolylineId("poly"),
           color: Color.fromARGB(255, 40, 122, 198),
           points: polylineCoordinates);
 
-      // add the constructed polyline as a set of points
-      // to the polyline set, which will eventually
-      // end up showing up on the map
       _polylines.add(polyline);
+      _polylines.add(polylinePath);
     });
   }
 
@@ -134,13 +118,11 @@ class _MyHomePageState extends State<MyHomePage> {
         bearing: CAMERA_BEARING,
         tilt: CAMERA_TILT,
         target: SOURCE_LOCATION);
-        
+
     requestPermission();
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
